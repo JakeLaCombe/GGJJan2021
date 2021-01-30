20173c395a8e16ca80b4a2a63aa326001e2c2c47
bryce <bryce.schmitendorf@gmail.com>
2021-01-30 16:01:50 -0600

    addedItemTypes

--- Assets/Scripts/QuestItem.cs
+++ Assets/Scripts/QuestItem.cs
@@ -5,16 +5,29 @@ using UnityEngine;
 public class QuestItem : MonoBehaviour
 {
     public string displayableName;
-
+    public ItemType Item;
+    public enum ItemType
+    {
+        CasioWatch,
+        FeatherBoa,
+        Keytar,
+        MotoHelmet,
+        ShutterShades,
+        VHS,
+        FluxCapacitor,
+        Tuxedo,
+        Jetpack,
+        BrickPhone
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
