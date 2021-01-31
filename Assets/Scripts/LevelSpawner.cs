7a79c08c5d40d45f06100666fec6bbeb6c12b3fb
Mike Nistler <bignis@gmail.com>
2021-01-31 14:09:02 -0600

    Go 6 rows up each time you return an item instead of 4 (small increase to difficulty)

--- Assets/Scripts/LevelSpawner.cs
+++ Assets/Scripts/LevelSpawner.cs
@@ -419,7 +419,7 @@ public class LevelSpawner : MonoBehaviour
         int minimumTileHeightToPutQuestItem = 5;
 
         // But it gets harder/higher every time you return a quest item!
-        const int heightIncreaseEveryReturnedItem = 4;
+        const int heightIncreaseEveryReturnedItem = 6;
         minimumTileHeightToPutQuestItem += (manager.NumberOfQuestItemsReturned * heightIncreaseEveryReturnedItem);
 
         // Find a good spot to put it
